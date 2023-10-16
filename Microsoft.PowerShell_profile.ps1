93522d4c8c12df705de793cf515a64fdeb1c59b1
Mikhail Sharapov <mishatoshi@gmail.com>
2023-10-16 11:50:38 +0300

    add profile psh

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+# Mojito Theme Collection readline configuration
+Set-PSReadlineOption -Color @{
+    Command            = 'Green'
+    Number             = 'Cyan'
+    String             = 'Yellow'
+    Member             = '#DCE775'
+    Operator           = '#FF8A65'
+    Type               = '#F06292'
+    Variable           = 'Gray'
+    Parameter          = '#FFB74D'
+    ContinuationPrompt = 'Gray'
+    Comment            = 'DarkGray'
+    Default            = 'Gray'
+    Error              = 'DarkRed'
+    Keyword            = '#FF8A65'
+}
